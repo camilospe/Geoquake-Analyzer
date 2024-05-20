3f30bbdeb93dd2bbcd065ba432f84445428dfb21
Camilo Silva Gutierrez <silvagutierrez1956@saskpolytech.ca>
2024-05-19 18:17:51 -0600

    Added functions for plotting and for displaying stats

--- earthquake_analyser.py
+++ earthquake_analyser.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json
 import numpy as np
 import earthquakes
+import matplotlib.pyplot as plt
 
 
 def read_dictionary(path="./earthquakes.geojson"):
@@ -13,21 +14,27 @@ def read_dictionary(path="./earthquakes.geojson"):
     :param path: path to a json file
     :return: dictionary with the information from the file
     """
+
+    # check if the user provided a path different to the default one
     if path != "./earthquakes.geojson":
         print(f"Received file path to analyze: {path}")
 
     path = Path(path)
 
+    # check if path directs to a file
     if path.exists():
+        # Try to load the json content to a dictionary
         try:
             geojson_dictionary = json.loads(path.read_text())
             print("loaded dictionary correctly")
+
         except Exception as e:
             print("Could not read provided file")
             raise e
     else:
         raise FileNotFoundError("File doesnt exist")
 
+    # return dictionary
     return geojson_dictionary
 
 
@@ -40,14 +47,223 @@ def load_quake_data_from_dictionary(dictionary):
     :param dictionary: Dictionary of earthquakes in geojson format
     :return: QuakeData object
     """
+
+    # create a new instance of the QuakeData class
     quake_data = earthquakes.QuakeData(dictionary)
-    number_of_valid_earthquakes = len(quake_data.get_filtered_list())
+
     # Check that at least one valid earthquake was found
+    number_of_valid_earthquakes = len(quake_data.get_filtered_list())
     if number_of_valid_earthquakes == 0:
         raise ValueError("No earthquakes found in the provided dictionary")
     else:
         print(f"Dictionary contained {number_of_valid_earthquakes} valid earthquakes")
     return quake_data
+
+
+def set_location_filter(quake_data):
+    """
+    This method will set a filter of how far the earthquakes can be from a specific location
+    :param quake_data: QuakeData object
+    """
+    print("""
+
+        To set the a new location filter please provide all of the following values:
+
+        Latitude: Decimal degrees latitude. Negative values for southern latitudes. Typical Values [-90.0, 90.0]
+        Longitude: Decimal degrees longitude. Negative values for western longitudes. Typical Values [-180.0, 180.0]
+        Distance: The maximum distance in kilometers to the (latitude, longitude) location Typical Values: [0, 10000]
+    """)
+
+    # get user parameter
+    user_latitude = input("Latitude: ")
+    user_longitude = input("Longitude: ")
+    user_distance = input("Distance: ")
+
+    # sanitize
+    user_latitude = user_latitude.strip()
+    user_longitude = user_longitude.strip()
+    user_distance = user_distance.strip()
+
+    # ensure the values are valid if it cant make the conversion and set filter it will display an error
+    try:
+        user_latitude = float(user_latitude)
+        user_longitude = float(user_longitude)
+        user_distance = float(user_distance)
+
+        quake_data.set_location_filter(user_latitude, user_longitude, user_distance)
+    except ValueError:
+        print("Please enter a valid latitude, longitude and distance values")
+
+
+def set_property_filter(quake_data):
+    """
+    This method will set up a property filter for the earthquakes
+    The user must provide at least one valid parameter
+    :param quake_data: QuakeData Object
+    """
+    print("""
+    
+        To set the a new property filter please provide at least one of the following values:
+        
+        Significance: A number describing how significant the event is. Larger numbers indicate a more significant event. Typical values: [0, 1000]
+        Felt: The total number of felt reports submitted to the DYFI? system. Typical Values: [0.0, 180.0]
+        Magnitude: The magnitude for the event. Typical Values: [-1.0, 10.0]
+        
+        If you wish to skip any of these values just press enter without any input
+    """)
+
+    # get parameters
+    user_significance = input("significance: ")
+    user_felt = input("felt: ")
+    user_magnitude = input("magnitude: ")
+
+    # Sanitize the input
+
+    user_significance = user_significance.strip()
+    user_felt = user_felt.strip()
+    user_magnitude = user_magnitude.strip()
+
+    invalid_count = 0
+
+    # verify if input is valid
+    for user_input in (user_significance, user_felt, user_magnitude):
+        try:
+            user_input = float(user_input)
+        except ValueError:
+            user_input = None
+            invalid_count += 1
+
+    # if the user didnt provide at least one parameter send error
+    if invalid_count >= 3:
+        print("No valid significance/felt/magnitude values found \nUnable to set property filter")
+
+    # if the user did provide at least one parameter set the filter
+    else:
+        quake_data.set_property_filter(user_magnitude, user_felt, user_significance)
+        print("Successfully set property filter")
+
+
+def clear_filters(quake_data):
+    """
+    This method will clear both the property and the location filters
+    :param quake_data: QuakeData object
+    """
+    try:
+        quake_data.clear_filter()
+        print("Successfully cleared filters")
+    except Exception as e:
+        print("Unable to clear filters")
+
+
+def display_filtered_quakes(quake_data):
+    """
+    This method will display the earthquakes after they were passed the location and property filters
+    :param quake_data: QuakeData Object
+    """
+
+    # display if at least one earthquake passes the filters
+    if len(quake_data.get_filtered_list()) > 0:
+        for quake in quake_data.get_filtered_list():
+            print(quake)
+    else:
+        print("No earthquakes records that passes current filters")
+
+
+def display_exceptional_quakes(quake_data):
+    """
+    The following method will display a list of earthquakes with a magnitude over 1 std deviation from the mean
+    :param quake_data:  QuakeData object
+    """
+    # get filtered array
+    filtered_array = quake_data.get_filtered_array()
+
+    # get mean and std dev
+    std = np.std(filtered_array['magnitude'])
+    mean = np.mean(filtered_array['magnitude'])
+
+    # get a list of earthquakes with a magnitude over 1 std deviation from the mean
+    exceptional_quakes = np.where(filtered_array['magnitude'] > (mean + std))
+    exceptional_quakes = filtered_array[exceptional_quakes]
+
+    # Display the list
+    for quake in exceptional_quakes['quake']:
+        print(quake)
+
+
+def display_magnitude_stats(quake_data):
+    """
+    This method will display the mean, standard deviation, mode, and median of the magnitude of the filtered earthquakes
+    :param quake_data: QuakeData object
+    """
+
+    # get filtered earthquakes
+    filtered_array = quake_data.get_filtered_array()
+
+    # calculate stats
+    mean = np.mean(filtered_array['magnitude'])
+    std = np.std(filtered_array['magnitude'])
+    median = np.median(filtered_array['magnitude'])
+
+    # round down the magnitude
+    round_mag = filtered_array['magnitude'].round().astype(int)
+
+    # calculate the mode
+    mode = np.argmax(np.bincount(round_mag))
+
+    # display stats
+    print("Magnitude Statistics")
+    print(f"Mean: {mean:.2f}")
+    print(f"Std Dev: {std:.2f}")
+    print(f"Median: {median:.2f}")
+    print(f"Mode: {mode}")
+
+
+def display_quake_map(quake_data):
+    """
+    This method will create a scatter map. Where the X is lat, Y is lon
+    and the size of the marker represents the magnitude of the earthquake(multiplied by 15 so its visible)
+    :param quake_data: QuakeData object
+    """
+    # force new window
+    plt.figure()
+
+    # get filtered array
+    filtered_array = quake_data.get_filtered_array()
+
+    # plot the scatter map
+    plt.scatter(filtered_array['lat'], filtered_array['long'], s=filtered_array['magnitude'] * 15,
+                edgecolors='black', marker='o',
+                label='Earthquake Magnitude')
+
+    plt.xlabel('Latitude (degrees)')
+    plt.ylabel('Longitude (degrees)')
+    plt.title('Earthquakes Magnitude')
+
+    plt.show(block=False)
+
+
+def display_magnitude_chart(quake_data):
+    """
+    This function will display a bar char where each bar will show the number of earthquakes for a magnitude (rounded down)
+    the scatter bar will be displayed in a new window.
+    :param quake_data: QuakeData object
+    """
+    # force new window
+    plt.figure()
+
+    # get array and round down
+    filtered_array = quake_data.get_filtered_array()
+    round_mag = filtered_array['magnitude'].round().astype(int)
+
+    # get the different magnitudes at their counts
+    magnitude, counts = np.unique(round_mag, return_counts=True)
+
+    # plot the  bar chart
+    plt.bar(magnitude, counts, color='cyan', edgecolor='black')
+    plt.title('Earthquake Magnitude')
+    plt.xlabel('Magnitude')
+    plt.ylabel('N of Earthquakes')
+    plt.show(block=False)
 
 
 def main(argv):
@@ -58,7 +274,7 @@ def main(argv):
     else:
         geojson_dictionary = read_dictionary()
 
-    load_quake_data_from_dictionary(geojson_dictionary)
+    quake_data = load_quake_data_from_dictionary(geojson_dictionary)
 
     while True:
         option = input("""
@@ -73,7 +289,28 @@ def main(argv):
            9. Quit
         Please select an option (1-9)
            """)
-        sys.exit()
+
+        option.strip().rstrip().lstrip()
+        if option == "1":
+            set_location_filter(quake_data)
+        elif option == "2":
+            set_property_filter(quake_data)
+        elif option == "3":
+            clear_filters(quake_data)
+        elif option == "4":
+            display_filtered_quakes(quake_data)
+        elif option == "5":
+            display_exceptional_quakes(quake_data)
+        elif option == "6":
+            display_magnitude_stats(quake_data)
+        elif option == "7":
+            display_quake_map(quake_data)
+        elif option == "8":
+            display_magnitude_chart(quake_data)
+        elif option == "9":
+            sys.exit()
+        else:
+            print("Invalid option, please select one of the options by choosing the number accordingly")
 
 
 if __name__ == "__main__":
--- earthquakes.py
+++ earthquakes.py
@@ -5,11 +5,10 @@ from pathlib import Path
 
 
 def ensure_numeric(value):
-    """This function will evaluate the value:
-    if value is an int or float, it will be returned
-    if value is a string it will try to convert to float:
-        if it fails will raise a TypeError
-        if it succeeds it will return the float value
+    """
+    This method will ensure a value is numeric
+    :param value: a value
+    :return: value if its numeric.
     """
     if isinstance(value, (int, float)):
         return value
@@ -20,13 +19,18 @@ def ensure_numeric(value):
 
 
 def calc_distance(lat1, lon1, lat2, lon2):
-    """This function will calculate the distance between two coordinates
+    """
+    This function will calculate the distance between two coordinates
     the coordinates are given through latitude and longitude (degrees).
     The distance is calculated used the haversine formula
-    The calculated distance will be in kilometers."""
-
+    The calculated distance will be in kilometers.
+    :param lat1: Latitude of the first point.
+    :param lon1: Longitude of the first point.
+    :param lat2: Latitude of the second point.
+    :param lon2: Longitude of the second point.
+    :return: distance in kilometers.
+    """
     # Ensure the coordinates are numeric
-
     lat1 = ensure_numeric(lat1)
     lon1 = ensure_numeric(lon1)
     lat2 = ensure_numeric(lat2)
@@ -34,6 +38,7 @@ def calc_distance(lat1, lon1, lat2, lon2):
 
     _earth_radius = 6_371_000
 
+    # apply haversine formula
     lat1_radian = lat1 * math.pi / 180
     lat2_radian = lat2 * math.pi / 180
 
@@ -46,13 +51,15 @@ def calc_distance(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     distance_meters = _earth_radius * c
-    distance_kms = distance_meters / 1000
 
+    # convert distance to kilometers
+    distance_kms = distance_meters / 1000
     return distance_kms
 
 
 def filter_invalid_earthquakes(earthquakes, magnitude_list, felt_list, significance_list, lat_list, long_list):
-    """This function receives a dictionary of earthquakes
+    """
+    This function receives a dictionary of earthquakes
     and discards the invalid ones. Valid earthquakes are those
     that meet the following criteria:
 
@@ -61,14 +68,30 @@ def filter_invalid_earthquakes(earthquakes, magnitude_list, felt_list, significa
     3. The coordinates are in a tuple format (validated by the coordinate_is_tuple function).
     4. The 'properties' dictionary contains the keys: 'mag', 'time', 'felt', 'sig', 'type', and 'magType'.
 
-    Valid earthquakes will be added in a list of Quake objects"""
+    Valid earthquakes will be added in a list of Quake objects
+    :param earthquakes: dictionary of earthquakes
+    :param magnitude_list: an empty list to populate with magnitudes
+    :param felt_list: an empty list to populate with felts
+    :param significance_list: an empty list to populate with significances
+    :param lat_list: an empty list to populate with latitudes
+    :param long_list: an empty list to populate with longitudes
+    :return: a list of valid earthquakes
+    """
 
     valid_earthquakes = []
 
+    # grab the earthquakes from the dictionary
     for earthquake in earthquakes['features']:
+
+        # valid earthquake must have the type, geometry -> type,
+        # mag, felt, sig, type, magType, and geometry -> coordinates fields
         if 'coordinates' in earthquake['geometry']:
             if earthquake['geometry']['type'] == "Point" and earthquake['type'] == "Feature":
+
+                # check if coordinates is a tuple of numbers
                 if coordinate_is_tuple(earthquake):
+
+                    # check no missing fields
                     if ('mag' in earthquake['properties'] and
                             'time' in earthquake['properties'] and
                             'felt' in earthquake['properties'] and
@@ -77,7 +100,10 @@ def filter_invalid_earthquakes(earthquakes, magnitude_list, felt_list, significa
                             'magType' in earthquake['properties'] and
                             isinstance(earthquake['properties']['felt'], (int, float))):
                         valid_earthquakes.append(earthquake)
+    # empty list to populate with valid earthquakes
     quakes_list = []
+
+    # populate earthquake and field lists
     for earthquake in valid_earthquakes:
         try:
             felt_list.append(int(earthquake['properties']['felt']))
@@ -92,15 +118,22 @@ def filter_invalid_earthquakes(earthquakes, magnitude_list, felt_list, significa
             lat_list.append(float(earthquake['geometry']['coordinates'][0]))
             long_list.append(float(earthquake['geometry']['coordinates'][1]))
 
+        # if operation fails, continue to next entry
         except ValueError as e:
             continue
+    # return earthquake list
     return quakes_list
 
 
 def coordinate_is_tuple(earthquake):
-    """This function will check if the coordinates are a valid tuple"""
+    """
+    This function will check if the coordinates are a valid tuple
+    :param earthquake: earthquake dictionary
+    :return: True if is a tuple of numeric values
+    """
     coordinates = earthquake['geometry']['coordinates']
 
+    # must be a tuple with 3 numbers
     if isinstance(coordinates, (tuple, list)) and len(coordinates) == 3:
         for coordinate in coordinates:
             if not isinstance(coordinate, (int, float)):
@@ -112,16 +145,18 @@ def coordinate_is_tuple(earthquake):
 
 class QuakeData:
     def __init__(self, earthquakes):
+
+        # empty field lists
         magnitude_list = []
         felt_list = []
         significance_list = []
         lat_list = []
         long_list = []
 
+        # set default filters
         self.location_filter = None
         self.property_filter = None
 
-        # this function will filter the earthquakes
         # create a list of Quake objects and update the list passed in the arguments
         quakes = filter_invalid_earthquakes(earthquakes, magnitude_list, felt_list,
                                             significance_list, lat_list, long_list)
@@ -151,48 +186,126 @@ class QuakeData:
             )
 
     def get_filtered_array(self):
+        """
+        This will filter the earthquakes based on the location and property filters
+        :return: np array of filtered earthquakes
+        """
 
+        # get all earthquakes
         filtered_array = self.quake_array
+
+        # if there is a location filter apply
         if self.location_filter is not None:
+            # vectorize function so its usable by numpy
             distance_vectorize = np.vectorize(calc_distance)
+
+            # np array with True if the location to the filter point is less or equal than the requested distance
             location_filter = np.where(
                 distance_vectorize(filtered_array['lat'], filtered_array['long'], self.location_filter[0],
                                    self.location_filter[1]) <= self.location_filter[2])
+
+            # update the array with the filter
             filtered_array = filtered_array[location_filter]
 
+        # if there is a property filter apply
         if self.property_filter is not None:
+            # np array with True if the felt, mag, and significance of the earthquakes are larger than the filter
             property_filter = np.where((filtered_array['felt'] >= self.property_filter[1]) &
                                        (filtered_array['magnitude'] >= self.property_filter[0]) &
                                        (filtered_array['significance'] >= self.property_filter[2]), True, False)
 
+            # apply filter
             filtered_array = filtered_array[property_filter]
 
+        # return np array after the two optional filters
         return filtered_array
 
     def get_filtered_list(self):
+        """
+        This function will return a list of Quake objects after they passed the filters
+        :return: list of Quake objects
+        """
+
+        # call get_filtered_array() to apply filters
         filtered_list = self.get_filtered_array()
+
+        # convert the 'quake' column to a list of Quakes
         filtered_list = filtered_list['quake']
         return filtered_list
 
     def set_location_filter(self, latitude, longitude, distance):
+        """
+        This function will set a maximum distance to a point for the earthquakes
+        :param latitude: Latitude of the point
+        :param longitude: longitude of the point
+        :param distance: Maximum distance to the point (kms)
+        """
+
+        # try to set the location filter (will need all parameters)
         try:
+            # ensure the values are numeric
+            ensure_numeric(latitude)
+            ensure_numeric(longitude)
+            ensure_numeric(distance)
+
             self.location_filter = (latitude, longitude, distance)
         except ValueError:
             raise ValueError("Invalid/Missing parameters")
 
     def set_property_filter(self, magnitude=None, felt=None, significance=None):
+        """
+        This function will set a minimum magnitude, felt, and significance criteria for the earthquakes
+        :param magnitude: magnitude of the earthquake
+        :param felt: number of reports
+        :param significance: number of how significant was the earthquake
+        """
+
+        # check if at least one parameter was correctly provided
         if magnitude is None and felt is None and significance is None:
             raise ValueError("Invalid/Missing parameters")
         else:
+
+            # parameters not provided are set to 0. also count the number of valid parameters
+            invalid_count = 0
             if magnitude is None:
                 magnitude = 0
+            else:
+                try:
+                    magnitude = float(magnitude)
+                except ValueError:
+                    magnitude = 0
+                    invalid_count += 1
+
             if felt is None:
                 felt = 0
+            else:
+                try:
+                    felt = float(felt)
+                except ValueError:
+                    felt = 0
+                    invalid_count += 1
+
             if significance is None:
                 significance = 0
-            self.property_filter = (magnitude, felt, significance)
+            else:
+                try:
+                    significance = float(significance)
+                except ValueError:
+                    significance = 0
+                    invalid_count += 1
+
+            # if all parameters are not valid then raise the exception
+            if invalid_count >= 3:
+                raise ValueError("Invalid/Missing parameters")
+
+            # set the filter
+            else:
+                self.property_filter = (magnitude, felt, significance)
 
     def clear_filter(self):
+        """
+        This function will clear the location and property filters
+        """
         self.location_filter = None
         self.property_filter = None
 
